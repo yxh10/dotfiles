bcbe2623fde904935b1af503c32f30ff72e60416
Steven Degutis <steven@cleancoders.com>
2013-08-02 01:34:13 -0500

    crap

--- home/zeph.go
+++ home/zeph.go
@@ -22,6 +22,8 @@ var respChans = make(map[uint64]chan interface{})
 func listenForCallbacks() {
 	reader := bufio.NewReader(c)
 	for {
+		// fmt.Println("about to read")
+
 		numBytes, _ := reader.ReadString('\n')
 		numBytes = strings.Trim(numBytes, "\n")
 		i, _ := strconv.ParseUint(numBytes, 10, 64)
@@ -49,10 +51,21 @@ func listenForCallbacks() {
 
 var msgid uint64 = 0
 func send(recv uint64, method string, args ...interface{}) interface{} {
+	lastarg := args[len(args)-1]
+
+	f1, isf1 := lastarg.(func())
+	f2, isf2 := lastarg.(func(float64))
+
+	f1 = f1
+	f2 = f2
+
+
+	fmt.Println("uhh", isf1, isf2)
+
+
 	msgid++
 
 	ch := make(chan interface{}, 10) // probably enough
-
 	respChans[msgid] = ch
 
 	msg := []interface{}{msgid, recv, method}
@@ -60,9 +73,21 @@ func send(recv uint64, method string, args ...interface{}) interface{} {
 	jsonstr := string(val)
 	fmt.Fprintf(c, "%v\n%v", len(jsonstr), jsonstr)
 
+	if isf1 || isf2 {
+		go func() {
+			<-ch
+		}()
+		fmt.Println("ONE")
+		return nil
+	}
+
+
+	fmt.Println("TWO")
 	resp := <-ch
 	delete(respChans, msgid)
 	return resp
+
+
 }
 
 
@@ -72,10 +97,14 @@ var API uint64 = 0
 
 
 func main() {
-	go func() {
-		contents := send(API, "alert", "hi", 1)
-		send(API, "alert", "there", 1)
-		fmt.Println(contents)
-	}()
+	send(API, "bind", "d", []string{"cmd", "shift"}, func() {
+		fmt.Println("called!")
+	})
+
+	// go func() {
+		// contents := send(API, "alert", "hi", 1)
+		// send(API, "alert", "there", 1)
+		// fmt.Println(contents)
+	// }()
 	listenForCallbacks()
 }
