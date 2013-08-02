d67dfccba607880f3a07a0f5e99e3d5932859477
Steven Degutis <steven@cleancoders.com>
2013-08-02 17:02:16 -0500

    fix

--- projs/zeph.py
+++ projs/zeph.py
@@ -1,39 +1,53 @@
 from twisted.internet import reactor, protocol
 import json
-import signal
-import sys
+
+# zephConn = None
 
 
-zephConn = None
 
-# json.loads(s)
-
+ONEYEAR = 365 * 24 * 60 * 60
 
 class EchoClient(protocol.Protocol):
     def connectionMade(self):
-        global zephConn
-        zephConn = self
-        s = json.dumps([0, 0, 'alert', 'hello world', 1])
-        self.transport.write(str(len(s)) + '\n' + s)
+        # global zephConn
+        # zephConn = self
+
+        self.buf = ''
+        self.readingSize = None
+
+        self.sendMsg([0, 0, 'bind', 'd', ['cmd', 'shift']])
+
+    def sendMsg(self, msg):
+        msgStr = json.dumps(msg)
+        self.transport.write(str(len(msgStr)) + '\n' + msgStr)
 
     def dataReceived(self, data):
-        print "Server said:", data
-        print zephConn
-        # self.transport.loseConnection()
+        self.buf += data
+        while self.processIncomingData():
+            pass
 
-    def connectionLost(self, reason):
-        print "connection lost"
+    def handleMessage(self, msg):
+        print 'msg:', msg
+
+    def processIncomingData(self):
+        if self.readingSize:
+            l = len(self.buf)
+            if l >= self.readingSize:
+                msg, self.buf = self.buf[:l], self.buf[l:]
+                obj = json.loads(msg)
+                self.handleMessage(obj)
+                self.readingSize = None
+                return True
+        else:
+            idx = self.buf.find('\n')
+            if idx != -1:
+                self.readingSize, self.buf = int(self.buf[:idx]), self.buf[idx+1:]
+                return True
+
+        return False
 
 class EchoFactory(protocol.ClientFactory):
     protocol = EchoClient
-
-    def clientConnectionFailed(self, connector, reason):
-        print "Connection failed - goodbye!"
-        # reactor.stop()
-
-    def clientConnectionLost(self, connector, reason):
-        print "Connection lost - goodbye!"
-        # reactor.stop()
 
 f = EchoFactory()
 reactor.connectTCP("localhost", 1235, f)
