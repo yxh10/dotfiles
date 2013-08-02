773d21cf5e5eb67d53ffcdb9c77345d453d66835
Steven Degutis <steven@cleancoders.com>
2013-08-02 10:06:52 -0500

    meh

--- home/zephyros/project.clj
+++ home/zephyros/project.clj
@@ -4,6 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.2"]
-                 [aleph "0.3.0-rc2"]]
-  :main zephyros.core)
+                 [org.clojure/data.json "0.2.2"]])
--- home/zephyros/src/zephyros/core.clj
+++ home/zephyros/src/zephyros/core.clj
@@ -1,13 +1,37 @@
 (ns zephyros.core
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json])
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader)))
 
-(use 'lamina.core 'aleph.tcp 'gloss.core)
+(def zephyros-server {:name "localhost" :port 1235})
+
+(declare conn-handler)
+
+(defn connect [server]
+  (let [socket (Socket. (:name server) (:port server))
+        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+        out (PrintWriter. (.getOutputStream socket))
+        conn (ref {:in in :out out})]
+    (doto (Thread. #(conn-handler conn)) (.start))
+    conn))
+
+(defn write [conn msg]
+  (doto (:out @conn)
+    (.println (str msg "\r"))
+    (.flush)))
+
+(defn conn-handler [conn]
+  (while (nil? (:exit @conn))
+    (let [msg (.readLine (:in @conn))]
+      (println msg)
+      (cond
+        (re-find #"^ERROR :Closing Link:" msg)
+        (dosync (alter conn merge {:exit true}))
+        (re-find #"^PING" msg)
+        (write conn (str "PONG "  (re-find #":.*" msg)))))))
 
 (defn -main []
   (let [s (json/write-str [1, 0, "alert", "hello", 2])
         size (count s)
-        ch (wait-for-result
-            (tcp-client {:host "localhost",
-                         :port 1235,}))]
-    (enqueue ch (format "%s\n%s", size, s))
-    (wait-for-message ch)))
+        conn (connect zephyros-server)]
+    (write conn (format "%s\n%s", size, s))))
