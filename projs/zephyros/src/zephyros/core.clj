95df92ac730fcc86b64d3152f3fc7f10053b6748
Steven Degutis <steven@cleancoders.com>
2013-08-03 02:37:29 -0500

    maybe

--- projs/zephyros/src/zephyros/core.clj
+++ projs/zephyros/src/zephyros/core.clj
@@ -6,10 +6,13 @@
   (bind "d" ["cmd" "shift"]
         (fn []
           (let [win (get-focused-window)]
-            (maximize win)
-            ;; (set-frame win
-            ;;            (-> (get-frame win)
-            ;;                (update-in [:w] - 20)))
+            ;; (maximize win)
+            (set-frame win
+                       (-> (get-frame win)
+                           (update-in [:x] + 20)
+                           (update-in [:y] + 20)
+                           (update-in [:w] - 40)
+                           (update-in [:h] - 40)))
 
             ;; (alert frame 1)
             )))
