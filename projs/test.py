d2645656f45396baa7367977435dd1faabd7c738
Steven Degutis <steven@cleancoders.com>
2013-08-05 13:34:46 -0500

    hmmmm

--- projs/test.py
+++ projs/test.py
@@ -13,29 +13,72 @@ s.connect(('127.0.0.1', 1235))
 
 q = Queue.Queue(10)
 
-def whatever():
+class DataReader:
+  def __init__(self):
+    self.buf = ''
+    self.readingSize = None
+
+  def processData(self):
+    if self.readingSize:
+      l = len(self.buf)
+      if l >= self.readingSize:
+        msg, self.buf = self.buf[:l], self.buf[l:]
+        obj = json.loads(msg)
+        self.readingSize = None
+        q.put(obj)
+        self.processData()
+    else:
+      idx = self.buf.find('\n')
+      if idx != -1:
+        self.readingSize = int(self.buf[:idx])
+        self.buf = self.buf[idx+1:]
+        self.processData()
+
+  def readForever(self):
     while True:
-        data = s.recv(4096)
-        q.put(data)
+      self.buf += s.recv(4096)
+      self.processData()
 
 
 
-t = threading.Thread(target=whatever)
+class DataWriter:
+  def __init__(self):
+    self.buf = ''
+
+  def queueData(self):
+    pass
+
+  def writeForever(self):
+    pass
+
+
+
+dw = DataWriter()
+t = threading.Thread(target=dw.writeForever)
 t.daemon = True
 t.start()
 
 
 
-msg = [0, 0, 'alert', 'hello world', 2]
-msgStr = json.dumps(msg)
-s.send(str(len(msgStr)) + '\n' + msgStr)
+dr = DataReader()
+t = threading.Thread(target=dr.readForever)
+t.daemon = True
+t.start()
+
+
+
+def sendMessage(msg):
+  msgStr = json.dumps(msg)
+  s.send(str(len(msgStr)) + '\n' + msgStr)
+
+sendMessage([0, 0, 'bind', 'd', ['cmd', 'shift']])
 
 
 
 
 try:
-    while True:
-        data = q.get(True, ONEYEAR)
-        print "received data:", data
+  while True:
+    msg = q.get(True, ONEYEAR)
+    print "got msg:", msg
 except KeyboardInterrupt:
-    print 'k bye'
+  print 'k bye'
