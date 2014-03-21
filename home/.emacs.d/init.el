047b3ad1f6e91ec3ae6533cb4cbdb6c80a2bbaec
Steven Degutis <steven@cleancoders.com>
2014-03-21 13:04:20 -0500

    vertical on its own

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -192,13 +192,22 @@
 
 
 (require 'ido-ubiquitous)
-(require 'ido-vertical-mode)
 
 ;; ido-mode
 (ido-mode 1)
 (ido-ubiquitous-mode 1)
-(ido-vertical-mode)
 (setq ido-default-file-method 'selected-window)
+
+
+;; make ido-mode vertical
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 
 
 (require 'smex)
