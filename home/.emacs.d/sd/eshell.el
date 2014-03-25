13037b3f49e1b6da216101b13644e1218e4b4ce9
Steven Degutis <steven@cleancoders.com>
2014-03-25 10:39:44 -0500

    uhhhh...

--- home/.emacs.d/sd/eshell.el
+++ home/.emacs.d/sd/eshell.el
@@ -1,11 +1,11 @@
 (add-hook 'eshell-mode-hook 'toggle-truncate-lines)
 (add-hook 'eshell-preoutput-filter-functions 'ansi-color-apply)
-(global-set-key (kbd "s-t") 'sd/open-new-eshell)
 (autoload 'chruby "chruby")
 
 (defun sd/open-new-eshell ()
   (interactive)
   (eshell 'ignored-value))
+(global-set-key (kbd "s-t") 'sd/open-new-eshell)
 
 (defun sd/eshell-search-history ()
   (interactive)
@@ -22,16 +22,5 @@
 (add-hook 'eshell-mode-hook
           (lambda ()
             (define-key eshell-mode-map (kbd "s-k") 'sd/clear-eshell-buffer)
-            (define-key eshell-mode-map (kbd "M-r") 'sd/eshell-search-history)))
-
-(add-hook 'eshell-mode-hook
-          (lambda ()
+            (define-key eshell-mode-map (kbd "M-r") 'sd/eshell-search-history)
             (setq pcomplete-cycle-completions nil)))
-
-
-(setq eshell-prompt-function (lambda ()
-                               (concat
-                                "\n"
-                                (abbreviate-file-name
-                                 (eshell/pwd))
-                                (if (= (user-uid) 0) " # " " $ "))))
