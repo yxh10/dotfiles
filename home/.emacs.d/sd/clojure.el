d69a099757fe09ef0af4bcd1111cf181b36e622d
Steven Degutis <steven@cleancoders.com>
2014-03-27 08:22:29 -0500

    more stuff

--- home/.emacs.d/sd/clojure.el
+++ home/.emacs.d/sd/clojure.el
@@ -28,4 +28,6 @@
      (require 'clojure-test-mode)))
 
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 (setq nrepl-hide-special-buffers t)
