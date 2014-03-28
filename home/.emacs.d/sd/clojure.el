2017607abbeea85f51730c61c3601a1b47d524d0
Steven Degutis <steven@cleancoders.com>
2014-03-28 12:43:38 -0500

    stop popping up

--- home/.emacs.d/sd/clojure.el
+++ home/.emacs.d/sd/clojure.el
@@ -30,4 +30,5 @@
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-(setq nrepl-hide-special-buffers t)
+(setq nrepl-hide-special-buffers t
+      cider-popup-stacktraces nil)
