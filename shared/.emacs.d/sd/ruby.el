da68622ee26328a23f2e07e6985991915107ed95
Steven Degutis <steven@cleancoders.com>
2014-04-03 23:16:03 -0500

    arch doesn't have rbenv (yet)

--- shared/.emacs.d/sd/ruby.el
+++ shared/.emacs.d/sd/ruby.el
@@ -1,5 +1,4 @@
-;; (add-hook 'ruby-mode-hook 'smartparens-mode)
-
-(require 'rbenv)
-(global-rbenv-mode 1)
-(setq rbenv-executable "/usr/local/bin/rbenv")
+(when (equal window-system 'ns)
+  (require 'rbenv)
+  (global-rbenv-mode 1)
+  (setq rbenv-executable "/usr/local/bin/rbenv"))
