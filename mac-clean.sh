af7a392ba0c6a02a44054c02f060aa4276cbe6ed
Steven Degutis <steven@cleancoders.com>
2014-04-03 21:36:25 -0500

    rename home to mac

--- mac-clean.sh
+++ mac-clean.sh
@@ -2,4 +2,4 @@
 set -ex
 
 rm -f $HOME/Library/KeyBindings/DefaultKeyBinding.dict
-find $PWD/home -mindepth 1 -maxdepth 1 -execdir rm -f $HOME/{} \;
+find $PWD/mac -mindepth 1 -maxdepth 1 -execdir rm -f $HOME/{} \;
--- mac-install.sh
+++ mac-install.sh
@@ -3,4 +3,4 @@ set -ex
 
 mkdir -p $HOME/Library/KeyBindings/
 cp $PWD/DefaultKeyBinding.dict $HOME/Library/KeyBindings/
-find $PWD/home -mindepth 1 -maxdepth 1 -exec ln -s {} $HOME/. \;
+find $PWD/mac -mindepth 1 -maxdepth 1 -exec ln -s {} $HOME/. \;
