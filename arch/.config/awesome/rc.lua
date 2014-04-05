dd80d7fc41def23716931467f59f561cc194844d
Steven Degutis <steven@cleancoders.com>
2014-04-04 23:34:25 -0500

    first awesome customization!

--- arch/.config/awesome/rc.lua
+++ arch/.config/awesome/rc.lua
@@ -353,7 +353,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 5,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
