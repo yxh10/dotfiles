9f6ec8fd78460a681f26dd5603daf417f1395ff6
Steven Degutis <steven@cleancoders.com>
2014-04-09 23:41:27 -0500

    this is getting ridiculous

--- shared/.emacs.d/sd/buffers.el
+++ shared/.emacs.d/sd/buffers.el
@@ -1,31 +1,19 @@
-;; buffer management
-(autoload 'buf-move-up "buffer-move" nil t)
-(autoload 'buf-move-down "buffer-move" nil t)
-(autoload 'buf-move-left "buffer-move" nil t)
-(autoload 'buf-move-right "buffer-move" nil t)
-(global-set-key (kbd "s-[") 'previous-buffer)
-(global-set-key (kbd "s-]") 'next-buffer)
-(global-unset-key (kbd "C-x C-b"))
-(global-set-key (kbd "s-{") '(lambda () (interactive) (other-window -1)))
-(global-set-key (kbd "s-}") 'other-window)
-(global-set-key (kbd "<C-s-up>")     'buf-move-up)
-(global-set-key (kbd "<C-s-down>")   'buf-move-down)
-(global-set-key (kbd "<C-s-left>")   'buf-move-left)
-(global-set-key (kbd "<C-s-right>")  'buf-move-right)
-
 ;; resize buffers
 (require 'windsize)
 (windsize-default-keybindings)
-
-;; makes Cmd-Shift-N open a new window with a blank buffer
-(defun sd/new-blank-bufer ()
-  (interactive)
-  (make-frame)
-  (switch-to-buffer (generate-new-buffer-name "untitled"))
-  (text-mode))
-(global-set-key (kbd "s-N") 'sd/new-blank-bufer)
 
 ;; make buffer names betterly unique
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (setq uniquify-strip-common-suffix t)
+
+(defun sd/new-blank-bufer ()
+  (interactive)
+  (make-frame)
+  (switch-to-buffer (generate-new-buffer-name "untitled"))
+  (text-mode))
+
+(autoload 'buf-move-up "buffer-move" nil t)
+(autoload 'buf-move-down "buffer-move" nil t)
+(autoload 'buf-move-left "buffer-move" nil t)
+(autoload 'buf-move-right "buffer-move" nil t)
--- shared/.emacs.d/sd/os.el
+++ shared/.emacs.d/sd/os.el
@@ -6,7 +6,37 @@
   (global-set-key (kbd "s-z") 'undo-tree-undo)
   (global-set-key (kbd "s-Z") 'undo-tree-redo)
   (setq locate-command "mdfind")         ;; use spotlight, not locate
-  (set-face-font 'default "Menlo-12.0")) ;; use nice font
+  (set-face-font 'default "Menlo-12.0")  ;; use nice font
 
+  ;; buffer management
+  (global-set-key (kbd "s-[") 'previous-buffer)
+  (global-set-key (kbd "s-]") 'next-buffer)
+  (global-unset-key (kbd "C-x C-b"))
+  (global-set-key (kbd "s-{") '(lambda () (interactive) (other-window -1)))
+  (global-set-key (kbd "s-}") 'other-window)
+  (global-set-key (kbd "<C-s-up>")     'buf-move-up)
+  (global-set-key (kbd "<C-s-down>")   'buf-move-down)
+  (global-set-key (kbd "<C-s-left>")   'buf-move-left)
+  (global-set-key (kbd "<C-s-right>")  'buf-move-right)
+
+  ;; makes Cmd-Shift-N open a new window with a blank buffer
+  (global-set-key (kbd "s-N") 'sd/new-blank-bufer))
+
+
+
+;; when in arch linux
 (when (equal window-system 'x)
-  (setq mouse-autoselect-window t))
+  (setq mouse-autoselect-window t)
+
+  ;; buffer management
+  (global-set-key (kbd "M-[") 'previous-buffer)
+  (global-set-key (kbd "M-]") 'next-buffer)
+  (global-unset-key (kbd "C-x C-b"))
+  (global-set-key (kbd "C-M-[") '(lambda () (interactive) (other-window -1)))
+  (global-set-key (kbd "C-M-]") 'other-window)
+  ;; (global-set-key (kbd "<C-s-up>")     'buf-move-up)
+  ;; (global-set-key (kbd "<C-s-down>")   'buf-move-down)
+  ;; (global-set-key (kbd "<C-s-left>")   'buf-move-left)
+  ;; (global-set-key (kbd "<C-s-right>")  'buf-move-right)
+
+  (global-set-key (kbd "C-c g") 'magit-status))
