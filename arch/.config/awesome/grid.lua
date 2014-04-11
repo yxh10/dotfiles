1788df9d66acf88c4dcd55992c3fd2bb31831623
Steven Degutis <steven@cleancoders.com>
2014-04-10 21:19:10 -0500

    window margins

--- arch/.config/awesome/grid.lua
+++ arch/.config/awesome/grid.lua
@@ -21,8 +21,16 @@ function grid.get_grid(w)
    return g
 end
 
+WINDOW_MARGIN = 3
+
 function grid.set_grid(w, grid)
    local screenRect = screen[1].workarea
+
+   screenRect.x = screenRect.x + WINDOW_MARGIN
+   screenRect.y = screenRect.y + WINDOW_MARGIN
+   screenRect.width = screenRect.width - (WINDOW_MARGIN * 2)
+   screenRect.height = screenRect.height - (WINDOW_MARGIN * 2)
+
    local thirdScreenWidth = screenRect.width / GRID_WIDTH
    local halfScreenHeight = screenRect.height / 2
 
@@ -36,11 +44,10 @@ function grid.set_grid(w, grid)
    newFrame.width = newFrame.width - (beautiful.border_width * 2)
    newFrame.height = newFrame.height - (beautiful.border_width * 2)
 
-   local margin = 0
-   newFrame.x = newFrame.x + margin
-   newFrame.y = newFrame.y + margin
-   newFrame.width = newFrame.width - (margin * 2)
-   newFrame.height = newFrame.height - (margin * 2)
+   newFrame.x = newFrame.x + WINDOW_MARGIN
+   newFrame.y = newFrame.y + WINDOW_MARGIN
+   newFrame.width = newFrame.width - (WINDOW_MARGIN * 2)
+   newFrame.height = newFrame.height - (WINDOW_MARGIN * 2)
 
    w:geometry(newFrame)
 end
