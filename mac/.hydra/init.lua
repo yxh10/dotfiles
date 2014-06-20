e8cf0cb7d454d43f6716c7da0af3cbc298d260fd
Steven Degutis <steven@cleancoders.com>
2014-06-20 12:11:10 -0500

    Using new "api" API.

--- mac/.hydra/init.lua
+++ mac/.hydra/init.lua
@@ -1,4 +1,4 @@
-hydra.alert("Hi! Wanna manage some windows?")
+api.alert("Hi! Wanna manage some windows?")
 
 local mash = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
@@ -12,7 +12,7 @@ local function round(num, idp)
   return math.floor(num * mult + 0.5) / mult
 end
 
-function hydra.window:getgrid()
+function api.window:getgrid()
   local winframe = self:frame()
   local screenrect = self:screen():frame_without_dock_or_menu()
   local thirdscreenwidth = screenrect.w / GRIDWIDTH
@@ -25,7 +25,7 @@ function hydra.window:getgrid()
   }
 end
 
-function hydra.window:setgrid(grid, screen)
+function api.window:setgrid(grid, screen)
   local screenrect = screen:frame_without_dock_or_menu()
   local thirdscreenwidth = screenrect.w / GRIDWIDTH
   local halfscreenheight = screenrect.h / 2
@@ -52,48 +52,48 @@ end
 
 local function adjustgridwidth(by)
   GRIDWIDTH = math.max(1, GRIDWIDTH + by)
-  hydra.alert("grid is now " .. tostring(GRIDWIDTH) .. " tiles wide", 1)
-  hydra.fn.map(hydra.window.visiblewindows(), snaptogrid)
+  api.alert("grid is now " .. tostring(GRIDWIDTH) .. " tiles wide", 1)
+  api.fn.map(api.window.visiblewindows(), snaptogrid)
 end
 
 local function adjust_focused_window(fn)
-  local win = hydra.window.focusedwindow()
+  local win = api.window.focusedwindow()
   local f = win:getgrid()
   fn(f)
   win:setgrid(f, win:screen())
 end
 
 local function opendictionary()
-  hydra.alert("Lexicon, at your service.", 1)
-  hydra.app.launchorfocus("Dictionary")
+  api.alert("Lexicon, at your service.", 1)
+  api.app.launchorfocus("Dictionary")
 end
 
-hydra.hotkey.bind(mash, 'D', opendictionary)
+api.hotkey.bind(mash, 'D', opendictionary)
 
-hydra.hotkey.bind(mash, ';', function() snaptogrid(hydra.window.focusedwindow()) end)
-hydra.hotkey.bind(mash, "'", function() hydra.fn.map(hydra.window.focusedwindow(), snaptogrid) end)
+api.hotkey.bind(mash, ';', function() snaptogrid(api.window.focusedwindow()) end)
+api.hotkey.bind(mash, "'", function() api.fn.map(api.window.focusedwindow(), snaptogrid) end)
 
-hydra.hotkey.bind(mash, '=', function() adjustgridwidth( 1) end)
-hydra.hotkey.bind(mash, '-', function() adjustgridwidth(-1) end)
+api.hotkey.bind(mash, '=', function() adjustgridwidth( 1) end)
+api.hotkey.bind(mash, '-', function() adjustgridwidth(-1) end)
 
-hydra.hotkey.bind(mashshift, 'H', function() hydra.window.focusedwindow():focuswindow_west() end)
-hydra.hotkey.bind(mashshift, 'L', function() hydra.window.focusedwindow():focuswindow_east() end)
-hydra.hotkey.bind(mashshift, 'K', function() hydra.window.focusedwindow():focuswindow_north() end)
-hydra.hotkey.bind(mashshift, 'J', function() hydra.window.focusedwindow():focuswindow_south() end)
+api.hotkey.bind(mashshift, 'H', function() api.window.focusedwindow():focuswindow_west() end)
+api.hotkey.bind(mashshift, 'L', function() api.window.focusedwindow():focuswindow_east() end)
+api.hotkey.bind(mashshift, 'K', function() api.window.focusedwindow():focuswindow_north() end)
+api.hotkey.bind(mashshift, 'J', function() api.window.focusedwindow():focuswindow_south() end)
 
 local function maximize_window()
-  local win = hydra.window.focusedwindow()
+  local win = api.window.focusedwindow()
   local f = {x = 0, y = 0, w = GRIDWIDTH, h = 2}
   win:setgrid(f, win:screen())
 end
 
 local function pushwindow_nextscreen()
-  local win = hydra.window.focusedwindow()
+  local win = api.window.focusedwindow()
   win:setgrid(win:getgrid(), win:screen():next())
 end
 
 local function pushwindow_prevscreen()
-  local win = hydra.window.focusedwindow()
+  local win = api.window.focusedwindow()
   win:setgrid(win:getgrid(), win:screen():previous())
 end
 
@@ -125,15 +125,15 @@ local function resizewindow_taller()
   adjust_focused_window(function(f) f.y = 0; f.h = 2 end)
 end
 
-hydra.hotkey.bind(mash, 'M', maximize_window)
-hydra.hotkey.bind(mash, 'N', pushwindow_nextscreen)
-hydra.hotkey.bind(mash, 'P', pushwindow_prevscreen)
+api.hotkey.bind(mash, 'M', maximize_window)
+api.hotkey.bind(mash, 'N', pushwindow_nextscreen)
+api.hotkey.bind(mash, 'P', pushwindow_prevscreen)
 
-hydra.hotkey.bind(mash, 'H', pushwindow_left)
-hydra.hotkey.bind(mash, 'L', pushwindow_right)
-hydra.hotkey.bind(mash, 'O', resizewindow_wider)
-hydra.hotkey.bind(mash, 'I', resizewindow_thinner)
+api.hotkey.bind(mash, 'H', pushwindow_left)
+api.hotkey.bind(mash, 'L', pushwindow_right)
+api.hotkey.bind(mash, 'O', resizewindow_wider)
+api.hotkey.bind(mash, 'I', resizewindow_thinner)
 
-hydra.hotkey.bind(mash, 'J', pushwindow_down)
-hydra.hotkey.bind(mash, 'K', pushwindow_up)
-hydra.hotkey.bind(mash, 'U', resizewindow_taller)
+api.hotkey.bind(mash, 'J', pushwindow_down)
+api.hotkey.bind(mash, 'K', pushwindow_up)
+api.hotkey.bind(mash, 'U', resizewindow_taller)
