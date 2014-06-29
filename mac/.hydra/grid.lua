116b984e2eee22d566856077628f36317d4af097
Steven Degutis <steven@cleancoders.com>
2014-06-29 00:30:59 -0500

    Fixed for new namespacing.

--- mac/.hydra/grid.lua
+++ mac/.hydra/grid.lua
@@ -1,18 +1,18 @@
-api.ext.grid = {}
+ext.grid = {}
 
-api.ext.grid.MARGINX = 5
-api.ext.grid.MARGINY = 5
-api.ext.grid.GRIDWIDTH = 3
+ext.grid.MARGINX = 5
+ext.grid.MARGINY = 5
+ext.grid.GRIDWIDTH = 3
 
 local function round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
 
-function api.ext.grid.get(win)
+function ext.grid.get(win)
   local winframe = win:frame()
   local screenrect = win:screen():frame_without_dock_or_menu()
-  local thirdscreenwidth = screenrect.w / api.ext.grid.GRIDWIDTH
+  local thirdscreenwidth = screenrect.w / ext.grid.GRIDWIDTH
   local halfscreenheight = screenrect.h / 2
   return {
     x = round((winframe.x - screenrect.x) / thirdscreenwidth),
@@ -22,9 +22,9 @@ function api.ext.grid.get(win)
   }
 end
 
-function api.ext.grid.set(win, grid, screen)
+function ext.grid.set(win, grid, screen)
   local screenrect = screen:frame_without_dock_or_menu()
-  local thirdscreenwidth = screenrect.w / api.ext.grid.GRIDWIDTH
+  local thirdscreenwidth = screenrect.w / ext.grid.GRIDWIDTH
   local halfscreenheight = screenrect.h / 2
   local newframe = {
     x = (grid.x * thirdscreenwidth) + screenrect.x,
@@ -33,73 +33,73 @@ function api.ext.grid.set(win, grid, screen)
     h = grid.h * halfscreenheight,
   }
 
-  newframe.x = newframe.x + api.ext.grid.MARGINX
-  newframe.y = newframe.y + api.ext.grid.MARGINY
-  newframe.w = newframe.w - (api.ext.grid.MARGINX * 2)
-  newframe.h = newframe.h - (api.ext.grid.MARGINY * 2)
+  newframe.x = newframe.x + ext.grid.MARGINX
+  newframe.y = newframe.y + ext.grid.MARGINY
+  newframe.w = newframe.w - (ext.grid.MARGINX * 2)
+  newframe.h = newframe.h - (ext.grid.MARGINY * 2)
 
   win:setframe(newframe)
 end
 
-function api.ext.grid.snap(win)
+function ext.grid.snap(win)
   if win:isstandard() then
-    api.ext.grid.set(win, api.ext.grid.get(win), win:screen())
+    ext.grid.set(win, ext.grid.get(win), win:screen())
   end
 end
 
-function api.ext.grid.adjustwidth(by)
-  api.ext.grid.GRIDWIDTH = math.max(1, api.ext.grid.GRIDWIDTH + by)
-  api.alert("grid is now " .. tostring(api.ext.grid.GRIDWIDTH) .. " tiles wide", 1)
-  api.fn.map(api.window.visiblewindows(), api.ext.grid.snap)
+function ext.grid.adjustwidth(by)
+  ext.grid.GRIDWIDTH = math.max(1, ext.grid.GRIDWIDTH + by)
+  hydra.alert("grid is now " .. tostring(ext.grid.GRIDWIDTH) .. " tiles wide", 1)
+  fnutils.map(window.visiblewindows(), ext.grid.snap)
 end
 
-function api.ext.grid.adjust_focused_window(fn)
-  local win = api.window.focusedwindow()
-  local f = api.ext.grid.get(win)
+function ext.grid.adjust_focused_window(fn)
+  local win = window.focusedwindow()
+  local f = ext.grid.get(win)
   fn(f)
-  api.ext.grid.set(win, f, win:screen())
+  ext.grid.set(win, f, win:screen())
 end
 
-function api.ext.grid.maximize_window()
-  local win = api.window.focusedwindow()
-  local f = {x = 0, y = 0, w = api.ext.grid.GRIDWIDTH, h = 2}
-  api.ext.grid.set(win, f, win:screen())
+function ext.grid.maximize_window()
+  local win = window.focusedwindow()
+  local f = {x = 0, y = 0, w = ext.grid.GRIDWIDTH, h = 2}
+  ext.grid.set(win, f, win:screen())
 end
 
-function api.ext.grid.pushwindow_nextscreen()
-  local win = api.window.focusedwindow()
-  api.ext.grid.set(win, api.ext.grid.get(win), win:screen():next())
+function ext.grid.pushwindow_nextscreen()
+  local win = window.focusedwindow()
+  ext.grid.set(win, ext.grid.get(win), win:screen():next())
 end
 
-function api.ext.grid.pushwindow_prevscreen()
-  local win = api.window.focusedwindow()
-  api.ext.grid.set(win, api.ext.grid.get(win), win:screen():previous())
+function ext.grid.pushwindow_prevscreen()
+  local win = window.focusedwindow()
+  ext.grid.set(win, ext.grid.get(win), win:screen():previous())
 end
 
-function api.ext.grid.pushwindow_left()
-  api.ext.grid.adjust_focused_window(function(f) f.x = math.max(f.x - 1, 0) end)
+function ext.grid.pushwindow_left()
+  ext.grid.adjust_focused_window(function(f) f.x = math.max(f.x - 1, 0) end)
 end
 
-function api.ext.grid.pushwindow_right()
-  api.ext.grid.adjust_focused_window(function(f) f.x = math.min(f.x + 1, api.ext.grid.GRIDWIDTH - f.w) end)
+function ext.grid.pushwindow_right()
+  ext.grid.adjust_focused_window(function(f) f.x = math.min(f.x + 1, ext.grid.GRIDWIDTH - f.w) end)
 end
 
-function api.ext.grid.resizewindow_wider()
-  api.ext.grid.adjust_focused_window(function(f) f.w = math.min(f.w + 1, api.ext.grid.GRIDWIDTH - f.x) end)
+function ext.grid.resizewindow_wider()
+  ext.grid.adjust_focused_window(function(f) f.w = math.min(f.w + 1, ext.grid.GRIDWIDTH - f.x) end)
 end
 
-function api.ext.grid.resizewindow_thinner()
-  api.ext.grid.adjust_focused_window(function(f) f.w = math.max(f.w - 1, 1) end)
+function ext.grid.resizewindow_thinner()
+  ext.grid.adjust_focused_window(function(f) f.w = math.max(f.w - 1, 1) end)
 end
 
-function api.ext.grid.pushwindow_down()
-  api.ext.grid.adjust_focused_window(function(f) f.y = 1; f.h = 1 end)
+function ext.grid.pushwindow_down()
+  ext.grid.adjust_focused_window(function(f) f.y = 1; f.h = 1 end)
 end
 
-function api.ext.grid.pushwindow_up()
-  api.ext.grid.adjust_focused_window(function(f) f.y = 0; f.h = 1 end)
+function ext.grid.pushwindow_up()
+  ext.grid.adjust_focused_window(function(f) f.y = 0; f.h = 1 end)
 end
 
-function api.ext.grid.resizewindow_taller()
-  api.ext.grid.adjust_focused_window(function(f) f.y = 0; f.h = 2 end)
+function ext.grid.resizewindow_taller()
+  ext.grid.adjust_focused_window(function(f) f.y = 0; f.h = 2 end)
 end
--- mac/.hydra/init.lua
+++ mac/.hydra/init.lua
@@ -1,13 +1,13 @@
 require "grid"
 
-api.alert "Hydra, at your service."
+hydra.alert "Hydra, at your service."
 
-api.pathwatcher.new(os.getenv("HOME") .. "/.hydra/", api.reload):start()
-api.autolaunch.set(true)
+pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+autolaunch.set(true)
 
-api.menu.show(function()
+menu.show(function()
     return {
-      {title = "About Hydra", fn = api.showabout},
+      {title = "About Hydra", fn = hydra.showabout},
       {title = "-"},
       {title = "Quit", fn = os.exit},
     }
@@ -17,38 +17,38 @@ local mash = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
 
 local function opendictionary()
-  api.alert("Lexicon, at your service.", 0.75)
-  api.application.launchorfocus("Dictionary")
+  hydra.alert("Lexicon, at your service.", 0.75)
+  application.launchorfocus("Dictionary")
 end
 
-api.hotkey.bind(mash, 'D', opendictionary)
+hotkey.bind(mash, 'D', opendictionary)
 
-api.hotkey.bind(mash, ';', function() api.ext.grid.snap(api.window.focusedwindow()) end)
-api.hotkey.bind(mash, "'", function() api.fnutils.map(api.window.visiblewindows(), api.ext.grid.snap) end)
+hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.snap) end)
 
-api.hotkey.bind(mash, '=', function() api.ext.grid.adjustwidth( 1) end)
-api.hotkey.bind(mash, '-', function() api.ext.grid.adjustwidth(-1) end)
+hotkey.bind(mash, '=', function() ext.grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
 
-api.hotkey.bind(mashshift, 'H', function() api.window.focusedwindow():focuswindow_west() end)
-api.hotkey.bind(mashshift, 'L', function() api.window.focusedwindow():focuswindow_east() end)
-api.hotkey.bind(mashshift, 'K', function() api.window.focusedwindow():focuswindow_north() end)
-api.hotkey.bind(mashshift, 'J', function() api.window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
 
-api.hotkey.bind(mash, 'M', api.ext.grid.maximize_window)
+hotkey.bind(mash, 'M', ext.grid.maximize_window)
 
-api.hotkey.bind(mash, 'N', api.ext.grid.pushwindow_nextscreen)
-api.hotkey.bind(mash, 'P', api.ext.grid.pushwindow_prevscreen)
+hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
 
-api.hotkey.bind(mash, 'J', api.ext.grid.pushwindow_down)
-api.hotkey.bind(mash, 'K', api.ext.grid.pushwindow_up)
-api.hotkey.bind(mash, 'H', api.ext.grid.pushwindow_left)
-api.hotkey.bind(mash, 'L', api.ext.grid.pushwindow_right)
+hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
+hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
+hotkey.bind(mash, 'H', ext.grid.pushwindow_left)
+hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
 
-api.hotkey.bind(mash, 'U', api.ext.grid.resizewindow_taller)
-api.hotkey.bind(mash, 'O', api.ext.grid.resizewindow_wider)
-api.hotkey.bind(mash, 'I', api.ext.grid.resizewindow_thinner)
+hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
+hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
+hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
 
-api.hotkey.bind(mash, 'X', api.logger.show)
-api.hotkey.bind(mash, "R", api.repl)
+hotkey.bind(mash, 'X', logger.show)
+hotkey.bind(mash, "R", hydra.repl)
 
-api.updates.check()
+updates.check()
